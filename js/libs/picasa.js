aa482d4bee6b0652d42aa1c7242e3ba7664b8b51
Hooker Juggernaut <secretgspot@gmail.com>
2013-06-24 03:46:23 -0600

    mm

--- js/libs/picasa.js
+++ js/libs/picasa.js
@@ -171,6 +171,9 @@
                 'google_ad_width = 300;'+
                 'google_ad_height = 250;'+
                 '//-->'+
+                '</script>'+
+                '<script type="text/javascript"'+
+                'src="http://pagead2.googlesyndication.com/pagead/show_ads.js">'+
                 '</script></span>')
             .append('<div style="clear:both"></div>');
 
